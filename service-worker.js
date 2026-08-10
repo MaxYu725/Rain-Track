128d9268e8bed538168dc65e0c572b8332e0bb83
MaxYu725 <maxyu0725@outlook.com>
2026-08-09 17:08:13 -0900

    Bump PWA shell for drawer top hotfix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'point-rain-pwa-v1.6.4-pwa2';
+const CACHE_VERSION = 'point-rain-pwa-v1.6.4-pwa3';
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const TILE_CACHE = `${CACHE_VERSION}-tiles`;
