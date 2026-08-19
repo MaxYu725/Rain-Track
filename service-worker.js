7df4e7bd01ce0babbdd05d8dace68253a6c9030f
MaxYu725 <maxyu0725@outlook.com>
2026-08-19 21:53:42 +0800

    chore: bump PWA shell for point explorer

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'point-rain-pwa-v1.6.4-pwa23';
+const CACHE_VERSION = 'point-rain-pwa-v1.6.4-pwa24';
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const TILE_CACHE = `${CACHE_VERSION}-tiles`;
